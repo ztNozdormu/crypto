2230c0af88ab9fe90ff3a8de065065fb3a6f01a0
luozijun <luozijun.assistant@gmail.com>
2020-08-06 18:58:53 +0800

    update sha256

--- src/sha2/mod.rs
+++ src/sha2/mod.rs
@@ -1,10 +1,13 @@
-// #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "sha"))]
-// #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "sha"))]
-
-
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(any(
+    all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "sha"),
+    all(target_arch = "aarch64", target_feature = "neon", target_feature = "crypto"),
+))]
 mod shani;
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+
+#[cfg(any(
+    all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "sha"),
+    all(target_arch = "aarch64", target_feature = "neon", target_feature = "crypto"),
+))]
 pub use self::shani::*;
 
 pub mod sha256;
