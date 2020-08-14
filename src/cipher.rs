9b5bf3dd454a3bea19b8c992d86de142c10f195c
luozijun <luozijun.assistant@gmail.com>
2020-08-14 19:20:07 +0800

    Update src/cipher.rs

--- src/cipher.rs
+++ src/cipher.rs
@@ -55,6 +55,7 @@ pub enum AeadStreamCipherKind {
 
 // ==============================  分组密码  ===============================
 pub trait BlockCipher: Sized {
+    const KIND: BlockCipherKind;
     const KEY_LEN: usize;
     const BLOCK_LEN: usize;
     
@@ -76,20 +77,18 @@ pub trait BlockCipher: Sized {
 
 
 // =============================  流密码  =============================
-
-// 流密码 流式数据加密
 pub trait StreamCipherEncrytor {
+    // 流密码 流式数据加密
     fn update(&mut self, plaintext: &[u8], ciphertext: &mut [u8]);
     fn finalize(self);
 }
-
-// 流密码 流式数据解密
 pub trait StreamCipherDecryptor {
+    // 流密码 流式数据解密
     fn update(&mut self, ciphertext: &[u8], plaintext: &mut [u8]);
     fn finalize(self);
 }
-
 pub trait StreamCipher: Sized {
+    const KIND: StreamCipherKind;
     const KEY_LEN: usize;
     const BLOCK_LEN: usize;
     const NONCE_LEN: usize;
@@ -123,31 +122,30 @@ pub trait AuthenticatedStreamCipherEncrytor {
     // NOTE: 追加 TAG 数据至 output 的结尾。
     fn finalize(self, tag: &mut [u8]);
 }
-
 pub trait AuthenticatedStreamCipherDecryptor {
     fn update(&mut self, ciphertext: &[u8], plaintext: &mut [u8]);
     // NOTE: 验证 TAG 数据是否吻合。
     fn finalize(self, tag: &[u8]) -> Result<(), ()>;
 }
-
 pub trait AuthenticatedStreamCipher: StreamCipher {
+    const KIND: AuthenticatedStreamCipherKind;
     const TAG_LEN: usize;
 
     type AeEncryptor: AuthenticatedStreamCipherEncrytor;
     type AeDecryptor: AuthenticatedStreamCipherDecryptor;
 
-    fn ae_encrypt_slice_oneshot(key: &[u8], nonce: &[u8], aad: &[u8], plaintext_in_and_ciphertext_out: &mut [u8]) {
+    fn ae_encrypt_slice_oneshot(key: &[u8], nonce: &[u8], plaintext_in_and_ciphertext_out: &mut [u8]) {
         let mut cipher = Self::new(key, nonce);
-        cipher.ae_encrypt_slice(aad, plaintext_in_and_ciphertext_out);
+        cipher.ae_encrypt_slice(plaintext_in_and_ciphertext_out);
     }
 
-    fn ae_decrypt_slice_oneshot(key: &[u8], nonce: &[u8], aad: &[u8], ciphertext_in_and_plaintext_out: &mut [u8]) {
+    fn ae_decrypt_slice_oneshot(key: &[u8], nonce: &[u8], ciphertext_in_and_plaintext_out: &mut [u8]) {
         let mut cipher = Self::new(key, nonce);
-        cipher.ae_decrypt_slice(aad, ciphertext_in_and_plaintext_out);
+        cipher.ae_decrypt_slice(ciphertext_in_and_plaintext_out);
     }
 
-    fn ae_encrypt_slice(&mut self, aad: &[u8], plaintext_in_and_ciphertext_out: &mut [u8]);
-    fn ae_decrypt_slice(&mut self, aad: &[u8], ciphertext_in_and_plaintext_out: &mut [u8]);
+    fn ae_encrypt_slice(&mut self, plaintext_in_and_ciphertext_out: &mut [u8]);
+    fn ae_decrypt_slice(&mut self, ciphertext_in_and_plaintext_out: &mut [u8]);
 
     fn ae_encrypt_stream(&self) -> Self::AeEncryptor;
     fn ae_decrypt_stream(&self) -> Self::AeDecryptor;
@@ -155,9 +153,39 @@ pub trait AuthenticatedStreamCipher: StreamCipher {
 
 
 // =================== 带有关联数据的认证加密（authenticated encryption with associated data, AEAD）==============
+pub trait AeadStreamCipherEncrytor {
+    fn update(&mut self, plaintext: &[u8], ciphertext: &mut [u8]);
+    // NOTE: 追加 TAG 数据至 output 的结尾。
+    fn finalize(self, tag: &mut [u8]);
+}
+pub trait AeadStreamCipherDecryptor {
+    fn update(&mut self, ciphertext: &[u8], plaintext: &mut [u8]);
+    // NOTE: 验证 TAG 数据是否吻合。
+    fn finalize(self, tag: &[u8]) -> Result<(), ()>;
+}
 pub trait AeadStreamCipher: AuthenticatedStreamCipher {
+    const KIND: AeadStreamCipherKind;
     const ID: u16;                 // IANA AEAD ID
     const NAME: &'static str;      // IANA AEAD Name
     const REFERENCE: &'static str; // IANA AEAD Reference
+
+    type AeadEncryptor: AeadStreamCipherEncrytor;
+    type AeadDecryptor: AeadStreamCipherDecryptor;
+
+    fn aead_encrypt_slice_oneshot(key: &[u8], nonce: &[u8], aad: &[u8], plaintext_in_and_ciphertext_out: &mut [u8]) {
+        let mut cipher = Self::new(key, nonce);
+        cipher.aead_encrypt_slice(aad, plaintext_in_and_ciphertext_out);
+    }
+
+    fn aead_decrypt_slice_oneshot(key: &[u8], nonce: &[u8], aad: &[u8], ciphertext_in_and_plaintext_out: &mut [u8]) {
+        let mut cipher = Self::new(key, nonce);
+        cipher.aead_decrypt_slice(aad, ciphertext_in_and_plaintext_out);
+    }
+
+    fn aead_encrypt_slice(&mut self, aad: &[u8], plaintext_in_and_ciphertext_out: &mut [u8]);
+    fn aead_decrypt_slice(&mut self, aad: &[u8], ciphertext_in_and_plaintext_out: &mut [u8]);
+
+    fn aead_encrypt_stream(&self) -> Self::AeadEncryptor;
+    fn aead_decrypt_stream(&self) -> Self::AeadDecryptor;
 }
 
