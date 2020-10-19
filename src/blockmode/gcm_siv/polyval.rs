ba537e16155b61c697fdd57174c7c12984728480
luozijun <luozijun.assistant@gmail.com>
2020-10-19 21:10:40 +0800

    update

--- src/blockmode/gcm_siv/polyval.rs
+++ src/blockmode/gcm_siv/polyval.rs
@@ -1,5 +1,5 @@
 
-// 代码参考:
+// 硬件加速代码参考:
 //      https://github.com/Shay-Gueron/AES-GCM-SIV/blob/master/AES_GCM_SIV_128/AES_GCM_SIV_128_Reference_Code/clmul_emulator.c
 
 #[path = "./polyval_generic.rs"]
